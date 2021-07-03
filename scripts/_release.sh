170fff36248de518e51b264c4c325b07804132ed
Lewis Daly <lewis@vesselstech.com>
2021-07-03 10:34:54 +0930

    feat(ci): add a new release script that handles pre-releases

--- scripts/_release.sh
+++ scripts/_release.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+# run a release for either `master` or `next` branch
+
+set -e
+set -u
+
+RELEASE_COMMIT_MESSAGE_FORMAT="chore(release): {{currentTag}} [skip ci]"
+
+
+case ${CIRCLE_BRANCH} in
+  # mainline release
+  master)
+    ./node_modules/.bin/standard-version \
+      --releaseCommitMessageFormat ${RELEASE_COMMIT_MESSAGE_FORMAT}
+  ;;
+
+  # prerelease
+  next)
+      ./node_modules/.bin/standard-version \
+        --prerelease alpha \
+        --releaseCommitMessageFormat ${RELEASE_COMMIT_MESSAGE_FORMAT}
+  ;;
+
+  *)
+    echo "tried to run a release for unknown branch: ${CIRCLE_BRANCH}"
+    exit 1
+  ;;
+
+esac
+
+# Push the tag so circleci will build the artifacts
+git push --follow-tags origin ${CIRCLE_BRANCH}
